3c2da6c7652d8d6f77e7de3bd232d4f165953ab0
HidDen-MaChinA <hei.franco.3@gmail.com>
2026-03-12 21:17:17 +0100

    feat: add test for best case that could happend to the authorization guard

---
+++ src/app/tests/guards/authorization.guard.test.ts
@@ -0,0 +1,51 @@
+import { AuthorizationGuard } from '../../../guards/authorization.guard';
+import { TestBed } from "@angular/core/testing";
+import {describe, expect, it, test, vi, beforeEach} from "vitest";
+import { AuthService } from '../../../service/auth-service.service';
+import { MockAuthServiceBestCase } from '../mocks/service/auth-service.service.mock';
+import { GuardResult, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+describe("Guard for authorization access to protected routes.", ()=>{
+  let authorizationGuard : AuthorizationGuard;
+  let authService: AuthService;
+  beforeEach(async ()=>{
+    const mockRouter = {
+      createUrlTree : vi.fn(),
+      navigate : vi.fn()
+    }
+    const httpClientMock = {
+      get: vi.fn()
+    }
+    await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AuthService, useValue: MockAuthServiceBestCase,
+        },
+        {
+          provide: Router, useValue: mockRouter
+        },{
+          provide: HttpClient, useValue: httpClientMock
+        }
+      ]
+    }).compileComponents()
+    authService = TestBed.inject(AuthService)
+    authorizationGuard = TestBed.inject(AuthorizationGuard)
+  })
+
+  it("should return true if whoami function from auth service return user authentified object.",async ()=>{
+    (authorizationGuard.canActivate() as Observable<GuardResult>).subscribe({
+      next(result){
+        expect(result).toBe(true);
+      }
+    });
+    (authorizationGuard.canActivateChild() as Observable<GuardResult>).subscribe({
+      next(result){
+        expect(result).toBe(true);
+      }
+    });
+  })
+})
