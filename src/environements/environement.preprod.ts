b2099a13b0361303f419583721f2e31e9bad2e59
HidDen-MaChinA <hei.franco.3@gmail.com>
2026-03-12 16:20:12 +0100

    feat: configuration for .env
    	modified:   .gitignore
    	modified:   angular.json
    	modified:   package.json
    	new file:   src/env.d.ts
    	new file:   src/environements/environement.preprod.ts
    	new file:   src/environements/environement.prod.ts
    	modified:   src/environements/environement.ts

---
+++ src/environements/environement.preprod.ts
@@ -0,0 +1,6 @@
+const apiUrl = import.meta.env["NG_APP_API_KEY"]
+
+export const environment = {
+  environement: "PREPROD",
+  apiUrl: apiUrl || "http://localhost:5000"
+}
--- src/environements/environement.prod.ts
+++ src/environements/environement.prod.ts
@@ -1,6 +1,6 @@
-const apiUrl = import.meta.env["NG_APP_API_URL"]
+const apiUrl = import.meta.env["NG_APP_API_KEY"]
 
 export const environment = {
-  environement: "DEV",
+  environement: "PROD",
   apiUrl: apiUrl || "http://localhost:5000"
 }
