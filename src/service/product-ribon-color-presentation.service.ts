40ad923ddc5af98ce496ad28cbf8fc9c2df35259
HidDen-MaChinA <hei.franco.3@gmail.com>
2026-04-02 12:39:12 +0300

    feat: add product api integration

--- src/service/product-ribon-color-presentation.service.ts
+++ src/service/product-ribon-color-presentation.service.ts
@@ -6,13 +6,23 @@ import { GetProductRibonColorPresentationType } from "../types/get-product-ribon
 export class ProductRibonColorPresentationService {
   constructor(private httpClient: HttpClient) {}
 
-  create(args: { productId: string; ribonColorSetId: string }) {
+  create(args: { image: File,productId: string; ribonColorSetId: string }) {
+    const {image, productId, ribonColorSetId} = args
+    const formData = new FormData();
+    formData.append('ribonColorSetId', ribonColorSetId);
+    formData.append('productId', productId)
+    formData.append('image', image, image.name)
     return this.httpClient.post<GetProductRibonColorPresentationType>(
       '/product-ribon-color-presentation/create',
-      args,
+      formData,
     );
   }
-  update(args: { id: string; ribonColorSetId: string }) {
+  update(args: {image: File, id: string; ribonColorSetId: string }) {
+    const {image, id, ribonColorSetId} = args
+    const formData = new FormData();
+    formData.append('ribonColorSetId', ribonColorSetId);
+    formData.append('id', id)
+    formData.append('image', image, image.name)
     return this.httpClient.patch<GetProductRibonColorPresentationType>(
       '/product-ribon-color-presentation/update',
       args,
---
+++ src/service/product.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { GetProductType } from '../types/get-product.type';
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+  constructor(private httpClient: HttpClient) {}
+  create(args: { thumbnail: File; description: string; name: string; price: number }) {
+    const { thumbnail, description, name, price } = args;
+    const formData = new FormData();
+    formData.append('thumbnail', thumbnail, thumbnail.name);
+    formData.append('description', description);
+    formData.append('name', name);
+    formData.append('price', price.toString());
+    return this.httpClient.post<GetProductType>('/product/create', formData);
+  }
+  update(args: { id: string; thumbnail: File; description: string; name: string; price: number }) {
+    const { thumbnail, id, description, name, price } = args;
+    const formData = new FormData();
+    formData.append('thumbnail', thumbnail, thumbnail.name);
+    formData.append('id', id);
+    formData.append('description', description);
+    formData.append('name', name);
+    formData.append('price', price.toString());
+    return this.httpClient.patch<GetProductType>('/product/update', formData);
+  }
+  getAllPaginated(query: { page: number; name: string }) {
+    return this.httpClient.get<GetProductType[]>('/product/get-all', {
+      params: query,
+    });
+  }
+  getOne(args: { id: string }) {
+    return this.httpClient.get<GetProductType>(`/product/get-one/${args.id}`);
+  }
+  delete(args: { id: string }) {
+    return this.httpClient.delete<{ message: string }>(`/product/delete/${args.id}`);
+  }
+}
